d5781a27b0e710daa7ec7be665dd0c400da5a7b1
Павел Жуков <33721692+LeaveMyYard@users.noreply.github.com>
2022-01-12 00:35:41 +0200

    Small fix

--- web3auth/__init__.py
+++ web3auth/__init__.py
@@ -1,5 +1,5 @@
 from .auth import AuthManager  # noqa
-from .types import DomainData, MessageData  # noqa
+from .types import DomainData, MessageData, AuthTokenPayload  # noqa
 from .exceptions import AuthError  # noqa
 from . import token  # noqa
 
